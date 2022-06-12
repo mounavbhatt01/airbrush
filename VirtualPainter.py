d6c374f3ead22d741ac5e000e56ae1c09de00128
Aditya <mailingadityavs@gmail.com>
2022-06-12 21:36:49 +0530

    Dayummm.

--- VirtualPainter.py
+++ VirtualPainter.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import time
 import HandTrackingModule as htm
+import pyvirtualcam
 
 Img=cv2.imread('1.png')
 cap=cv2.VideoCapture(0)
@@ -16,59 +17,64 @@ brushthickness=15
 
 detector=htm.handDetector()
 
+with pyvirtualcam.Camera(width=1280, height=720, fps=30) as cam:
+    while True:
+        
+        success,img=cap.read()
+        img=cv2.flip(img,1)
 
-while True:
-    success,img=cap.read()
-    img=cv2.flip(img,1)
+        img[0:125,0:1280]=Img
 
-    img[0:125,0:1280]=Img
+        img=detector.findHands(img)
+        lmList=detector.findPosition(img)
+        if len(lmList)!=0:
 
-    img=detector.findHands(img)
-    lmList=detector.findPosition(img)
-    if len(lmList)!=0:
+            x1,y1=lmList[8][1:]
+            x2,y2=lmList[12][1:]
 
-        x1,y1=lmList[8][1:]
-        x2,y2=lmList[12][1:]
+            fingers=detector.fingersUp()
 
-        fingers=detector.fingersUp()
+            if fingers[1] and fingers[2]:
+                xp, yp = 0, 0
+                print("selection")
+                if y1<125:
+                    if 250<x1<450:
+                        drawColor=(255,0,255)
+                    elif 550<x1<750:
+                        drawColor=(255,0,0)
+                    elif 800<x1<950:
+                        drawColor=(0,255,0)
+                    elif 1050<x1<1280:
+                        drawColor=(0,0,0)
 
-        if fingers[1] and fingers[2]:
-            xp, yp = 0, 0
-            print("selection")
-            if y1<125:
-                if 250<x1<450:
-                    drawColor=(255,0,255)
-                elif 550<x1<750:
-                    drawColor=(255,0,0)
-                elif 800<x1<950:
-                    drawColor=(0,255,0)
-                elif 1050<x1<1280:
-                    drawColor=(0,0,0)
+                cv2.rectangle(img, (x1, y1 - 25), (x2, y2 - 25), drawColor, cv2.FILLED)
 
-            cv2.rectangle(img, (x1, y1 - 25), (x2, y2 - 25), drawColor, cv2.FILLED)
+            if fingers[1] and fingers[2]==False:
+                cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
+                print("drawing")
+                if xp==0 and yp==0:
+                    xp,yp=x1,y1
 
-        if fingers[1] and fingers[2]==False:
-            cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
-            print("drawing")
-            if xp==0 and yp==0:
+                if drawColor==(0,0,0):
+                    cv2.line(img, (xp, yp), (x1, y1), drawColor,50)
+                    cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, 50)
+                else:
+                    cv2.line(img,(xp,yp),(x1,y1),drawColor,brushthickness)
+                    cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushthickness)
                 xp,yp=x1,y1
 
-            if drawColor==(0,0,0):
-                cv2.line(img, (xp, yp), (x1, y1), drawColor,50)
-                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, 50)
-            else:
-                cv2.line(img,(xp,yp),(x1,y1),drawColor,brushthickness)
-                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushthickness)
-            xp,yp=x1,y1
-
-    imgGray=cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY)
-    _,imgInv=cv2.threshold(imgGray,50,255,cv2.THRESH_BINARY_INV)
-    imgInv=cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
-    img=cv2.bitwise_and(img,imgInv)
-    img=cv2.bitwise_or(img,imgCanvas)
+        imgGray=cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY)
+        _,imgInv=cv2.threshold(imgGray,50,255,cv2.THRESH_BINARY_INV)
+        imgInv=cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
+        img=cv2.bitwise_and(img,imgInv)
+        img=cv2.bitwise_or(img,imgCanvas)
 
 
 
-    cv2.imshow('Image',img)
-    #cv2.imshow('Imageq', imgCanvas)
-    cv2.waitKey(1)
+        # cv2.imshow('Image',img)
+        # cv2.imshow('Imageq', imgCanvas)
+        # cv2.waitKey(1)
+
+        cam.send(img)
+        cam.sleep_until_next_frame()
+        cam.sleep_until_next_frame()
