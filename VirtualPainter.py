8ea78745ebe36a66469c8761a55a6f34f174228a
mounavbhatt01 <61976908+mounavbhatt01@users.noreply.github.com>
2022-06-13 16:30:22 +0530

    Update VirtualPainter.py

--- VirtualPainter.py
+++ VirtualPainter.py
@@ -9,11 +9,13 @@ cap=cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
 
+flag=False
+
 imgCanvas=np.zeros((720,1280,3),np.uint8)
 
-drawColor=(255,0,255)
+drawColor=(0,0,0)
 xp,yp=0,0
-brushthickness=15
+brushthickness=25
 
 detector=htm.handDetector()
 
@@ -33,35 +35,48 @@ with pyvirtualcam.Camera(width=1280, height=720, fps=30) as cam:
             x2,y2=lmList[12][1:]
 
             fingers=detector.fingersUp()
+            
+            if fingers[1] and fingers[4] and fingers[2] == 0 and fingers[3] == 0:
+                flag = not flag
 
-            if fingers[1] and fingers[2]:
-                xp, yp = 0, 0
-                print("selection")
-                if y1<125:
-                    if 250<x1<450:
-                        drawColor=(255,0,255)
-                    elif 550<x1<750:
-                        drawColor=(255,0,0)
-                    elif 800<x1<950:
-                        drawColor=(0,255,0)
-                    elif 1050<x1<1280:
-                        drawColor=(0,0,0)
+            if flag:
+            
+                if fingers[1] and fingers[2]:
+                    xp, yp = 0, 0
+                    print("selection")
+                    if y1<125:
+                        if 250<x1<450:
+                            drawColor=(255,0,255)
+                        elif 550<x1<750:
+                            drawColor=(255,0,0)
+                        elif 800<x1<950:
+                            drawColor=(0,255,0)
+                        elif 1050<x1<1280:
+                            drawColor=(0,0,0)
 
-                cv2.rectangle(img, (x1, y1 - 25), (x2, y2 - 25), drawColor, cv2.FILLED)
+                    cv2.rectangle(img, (x1, y1 - 25), (x2, y2 - 25), drawColor, cv2.FILLED)
 
-            if fingers[1] and fingers[2]==False:
-                cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
-                print("drawing")
-                if xp==0 and yp==0:
+                if fingers[1] and fingers[2]==False:
+                    cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
+                    print("drawing")
+                    if xp==0 and yp==0:
+                        xp,yp=x1,y1
+
+                    if drawColor==(0,0,0):
+                        cv2.line(img, (xp, yp), (x1, y1), drawColor,50)
+                        cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, 50)
+                    else:
+                        cv2.line(img,(xp,yp),(x1,y1),drawColor,brushthickness)
+                        cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushthickness)
                     xp,yp=x1,y1
+            if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 1:
+                imgCanvas = np.zeros((720, 1280, 3), np.uint8)
+                flag = False
+            # if fingers[1] and fingers[4] and fingers[2]==0 and fingers[3]==0:
+            #     flag=True
+            # if flag:
+            #     img[0:125, 0:1280] = Img
 
-                if drawColor==(0,0,0):
-                    cv2.line(img, (xp, yp), (x1, y1), drawColor,50)
-                    cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, 50)
-                else:
-                    cv2.line(img,(xp,yp),(x1,y1),drawColor,brushthickness)
-                    cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushthickness)
-                xp,yp=x1,y1
 
         imgGray=cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY)
         _,imgInv=cv2.threshold(imgGray,50,255,cv2.THRESH_BINARY_INV)
@@ -69,8 +84,11 @@ with pyvirtualcam.Camera(width=1280, height=720, fps=30) as cam:
         img=cv2.bitwise_and(img,imgInv)
         img=cv2.bitwise_or(img,imgCanvas)
 
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img=cv2.flip(img,1)
 
-
+        
+        
         # cv2.imshow('Image',img)
         # cv2.imshow('Imageq', imgCanvas)
         # cv2.waitKey(1)
