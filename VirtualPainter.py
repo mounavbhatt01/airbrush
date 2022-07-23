39068223a082981acad10839899ca3cdb0e2480f
Aditya <mailingadityavs@gmail.com>
2022-07-23 12:39:43 +0530

    KMN.

--- VirtualPainter.py
+++ VirtualPainter.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
-import time
 import HandTrackingModule as htm
 import pyvirtualcam
 
 Img=cv2.imread('1.png')
+Img = cv2.flip(Img,1)
+
 cap=cv2.VideoCapture(0)
+
 cap.set(3,1280)
 cap.set(4,720)
 
@@ -15,6 +17,7 @@ imgCanvas=np.zeros((720,1280,3),np.uint8)
 
 drawColor=(0,0,0)
 xp,yp=0,0
+
 brushthickness=25
 
 detector=htm.handDetector()
@@ -24,58 +27,58 @@ with pyvirtualcam.Camera(width=1280, height=720, fps=30) as cam:
         
         success,img=cap.read()
         img=cv2.flip(img,1)
-
-        img[0:125,0:1280]=Img
-
         img=detector.findHands(img)
         lmList=detector.findPosition(img)
+
+        # img[0:125,0:1280]=Img
+        
         if len(lmList)!=0:
 
             x1,y1=lmList[8][1:]
             x2,y2=lmList[12][1:]
 
             fingers=detector.fingersUp()
-            
-            if fingers[1] and fingers[4] and fingers[2] == 0 and fingers[3] == 0:
-                flag = not flag
 
+            
+            if fingers[1] and fingers[2]:
+                xp, yp = 0, 0
+                print("selection")
+                if y1<125:
+                    if x1<150: 
+                        flag = not flag
+                    elif 250<x1<450:
+                        drawColor=(255,0,255)
+                    elif 550<x1<750:
+                        drawColor=(255,0,0)
+                    elif 800<x1<950:
+                        drawColor=(0,255,0)
+                    elif 1050<x1<1280:
+                        drawColor=(0,0,0)
+
+                cv2.rectangle(img, (x1, y1 - 25), (x2, y2 - 25), drawColor, cv2.FILLED)
+            
             if flag:
-            
-                if fingers[1] and fingers[2]:
-                    xp, yp = 0, 0
-                    print("selection")
-                    if y1<125:
-                        if 250<x1<450:
-                            drawColor=(255,0,255)
-                        elif 550<x1<750:
-                            drawColor=(255,0,0)
-                        elif 800<x1<950:
-                            drawColor=(0,255,0)
-                        elif 1050<x1<1280:
-                            drawColor=(0,0,0)
-
-                    cv2.rectangle(img, (x1, y1 - 25), (x2, y2 - 25), drawColor, cv2.FILLED)
-
                 if fingers[1] and fingers[2]==False:
                     cv2.circle(img,(x1,y1),15,drawColor,cv2.FILLED)
                     print("drawing")
                     if xp==0 and yp==0:
                         xp,yp=x1,y1
 
-                    if drawColor==(0,0,0):
-                        cv2.line(img, (xp, yp), (x1, y1), drawColor,50)
-                        cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, 50)
-                    else:
-                        cv2.line(img,(xp,yp),(x1,y1),drawColor,brushthickness)
-                        cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushthickness)
+                    cv2.line(img, (xp, yp), (x1, y1), drawColor,brushthickness)
+                    cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushthickness)
                     xp,yp=x1,y1
-            if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 1:
+
+                    # if drawColor==(0,0,0):
+                    #     cv2.line(img, (xp, yp), (x1, y1), drawColor,50)
+                    #     cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, 50)
+                    # else:
+                    #     cv2.line(img,(xp,yp),(x1,y1),drawColor,brushthickness)
+                    #     cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushthickness)
+                    # xp,yp=x1,y1
+            
+            if fingers[0] and fingers[1] and fingers[2] and fingers[3] and fingers[4]:
                 imgCanvas = np.zeros((720, 1280, 3), np.uint8)
                 flag = False
-            # if fingers[1] and fingers[4] and fingers[2]==0 and fingers[3]==0:
-            #     flag=True
-            # if flag:
-            #     img[0:125, 0:1280] = Img
 
 
         imgGray=cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY)
@@ -86,13 +89,13 @@ with pyvirtualcam.Camera(width=1280, height=720, fps=30) as cam:
 
         img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         img=cv2.flip(img,1)
+        img[0:125,0:1280]=Img
+
 
         
         
         # cv2.imshow('Image',img)
-        # cv2.imshow('Imageq', imgCanvas)
         # cv2.waitKey(1)
 
         cam.send(img)
-        cam.sleep_until_next_frame()
         cam.sleep_until_next_frame()
